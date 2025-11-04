a67e5aa28728be6c109b4308356657d83fefebe0
xai bandala <xaiglennbandala@gmail.com>
2025-11-04 14:29:34 +0800

    logo added

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -2,7 +2,10 @@
 
 @section('content')
     <div class="d-flex justify-content-between align-items-center mb-3">
-        <h3 class="mb-0">AMA Students</h3>
+        <div class="d-flex align-items-center">
+            <img src="{{ asset('images/ama2.png') }}" alt="AMA Logo" class="me-2" style="height:40px;">
+            <h3 class="mb-0">AMA Students</h3>
+        </div>
         <div class="d-flex gap-2">
             <a href="{{ route('students.batch-create') }}" class="btn btn-success">Batch Add</a>
             <a href="{{ route('students.create') }}" class="btn btn-primary">Add Student</a>
